59f087c8974ff84e8abc7aa7a3e2514a7fedacb1
Joel Silva <53149347+zoreu@users.noreply.github.com>
2025-10-21 00:34:24 -0300

    Update generate_adblock.py

--- generate_adblock.py
+++ generate_adblock.py
@@ -24,7 +24,7 @@ def main():
     for line in raw:
         if line.startswith("!") or line.startswith("#"):
             if 'netcine' in line.lower() and add_ntc:
-                new_line = f"! netcine\n|http*://$popup,script,third-party,xmlhttprequest,domain={ntc}"
+                new_line = f"! netcine\n|http*://$popup,script,third-party,xmlhttprequest,domain={ntc}\n*$script,third-party,domain={ntc},important\n*$popup,domain={ntc},important"
                 print('Adicionando netcine: ', new_line)
                 filters.append(new_line)
                 add_ntc = False
